7cb2564c8179e299c3e41523c0632c6df1169ac5
nuuuk72-gif <nuuuk72@gmail.com>
2025-12-22 22:45:50 +0900

    Update style

---
+++ analyze_jobs.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+# -----------------------------------------------
+# 1. pandasでcsv読み込み確認
+# -----------------------------------------------
+df = pd.read_csv("C:/Users/user/my_project/python/korea_job_scraper/jobs.csv", encoding="utf-8")
+print(df.head())
+print("件数:", len(df))
+print(df.columns)
+print(df.head())
