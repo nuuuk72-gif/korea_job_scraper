169c22665d6640b6023170e21e8c271845e41321
nuuuk72-gif <nuuuk72@gmail.com>
2026-01-18 11:23:03 +0900

    Update style

--- analyze_jobs.py
+++ analyze_jobs.py
@@ -4,7 +4,8 @@ import pandas as pd
 # 1. pandasでcsv読み込み確認
 # -----------------------------------------------
 df = pd.read_csv("C:/Users/user/my_project/python/korea_job_scraper/jobs.csv", encoding="utf-8")
-print(df.head())
-print("件数:", len(df))
-print(df.columns)
-print(df.info())
+korea_count = df["タグ"].str.contains("韓国語").sum()
+print(f"韓国語が含まれる求人数: {korea_count}")
+
+korea_jobs = df[df["タグ"].str.contains("韓国語")]
+print(korea_jobs)
