f7e78cd07298d98b3892685537409fe19ebe102e
nuuuk72-gif <nuuuk72@gmail.com>
2026-01-21 21:29:34 +0900

    Update style

--- analyze_jobs.py
+++ analyze_jobs.py
@@ -4,8 +4,20 @@ import pandas as pd
 # 1. pandasでcsv読み込み確認
 # -----------------------------------------------
 df = pd.read_csv("C:/Users/user/my_project/python/korea_job_scraper/jobs.csv", encoding="utf-8")
+
+# -----------------------------------------------
+# 2. 韓国に関する求人の抽出
+# -----------------------------------------------
 korea_count = df["タグ"].str.contains("韓国語").sum()
 print(f"韓国語が含まれる求人数: {korea_count}")
 
 korea_jobs = df[df["タグ"].str.contains("韓国語")]
 print(korea_jobs)
+
+# -----------------------------------------------
+# 3. 韓国に関する求人の割合計算
+# -----------------------------------------------
+total_jobs = len(df)
+korea_ratio = korea_count / total_jobs * 100
+print(f"韓国語求人の割合: {korea_ratio:.2f}%")
+
