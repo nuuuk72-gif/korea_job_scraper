b3751fc543599b75a50a082ab5587a319d97f67e
nuuuk72-gif <nuuuk72@gmail.com>
2026-01-24 11:05:45 +0900

    Update style

--- analyze_jobs.py
+++ analyze_jobs.py
@@ -6,7 +6,7 @@ import pandas as pd
 df = pd.read_csv("C:/Users/user/my_project/python/korea_job_scraper/jobs.csv", encoding="utf-8")
 
 # -----------------------------------------------
-# 2. 韓国に関する求人の抽出
+# 2. 韓国に関する求人分析
 # -----------------------------------------------
 korea_count = df["タグ"].str.contains("韓国語").sum()
 print(f"韓国語が含まれる求人数: {korea_count}")
@@ -14,10 +14,20 @@ print(f"韓国語が含まれる求人数: {korea_count}")
 korea_jobs = df[df["タグ"].str.contains("韓国語")]
 print(korea_jobs)
 
-# -----------------------------------------------
-# 3. 韓国に関する求人の割合計算
-# -----------------------------------------------
 total_jobs = len(df)
 korea_ratio = korea_count / total_jobs * 100
 print(f"韓国語求人の割合: {korea_ratio:.2f}%")
 
+
+# -----------------------------------------------
+# 3. 各タグの出現回数カウント
+# -----------------------------------------------
+df["タグリスト"] = df["タグ"].str.split(",")
+
+tags_series = df["タグリスト"].explode()
+tags_series = tags_series.str.strip()
+
+tag_counts = tags_series.value_counts()
+
+print(tag_counts)
+print(tag_counts.head(10))
